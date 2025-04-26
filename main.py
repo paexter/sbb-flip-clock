f8de62c83cf25ec04735844a9420d3cb308690d6
Pascal Bérard <pas.berard@gmail.com>
2025-04-26 21:21:58 +0200

    Added clock to main.py

--- main.py
+++ main.py
@@ -2,11 +2,28 @@
 # requires-python = ">=3.11"
 # dependencies = ["pyserial==3.5"]
 # ///
-from sbb_fallblatt.panel_mock import parse_args, MockPanel
+
+from sbb_fallblatt import sbb_rs485
+import time
+from datetime import datetime
+
+SBB_MODULE_ADDR_HOUR = 82
+SBB_MODULE_ADDR_MIN = 29
 
 
 def main() -> None:
-    print("Hello from main.py!")
+    print("Starting clock.py!")
+
+    clock = sbb_rs485.PanelClockControl(
+        addr_hour=SBB_MODULE_ADDR_HOUR, addr_min=SBB_MODULE_ADDR_MIN
+    )
+    clock.connect()
+
+    while True:
+        clock.set_time_now()
+        ts = datetime.utcnow()
+        sleeptime = 60 - (ts.second + ts.microsecond / 1000000.0)
+        time.sleep(sleeptime)
 
 
 if __name__ == "__main__":
