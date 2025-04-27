1b7a5743450deeccc67a82be000d7fbe75b5884e
Pascal Bérard <pas.berard@gmail.com>
2025-04-27 13:38:09 +0200

    Added separate clock task

--- main.py
+++ main.py
@@ -6,30 +6,36 @@
 from sbb_fallblatt import sbb_rs485
 import time
 from datetime import datetime
+import threading
+from signal import pause
 
 from gpiozero import MotionSensor
 
-PIR_SENSOR_PIN = 23  # Pin 16 on the board
 
-
-def motion_function():
+def motion_function() -> None:
     print("Motion Detected")
-
-
-pir = MotionSensor(PIR_SENSOR_PIN)
-pir.when_motion = motion_function
 
 
 # import RPi.GPIO as GPIO
 # GPIO.setmode(GPIO.BCM)  # Use Broadcom (BCM) pin numbering
 # GPIO.setup(PIR_SENSOR_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
+PIR_SENSOR_PIN = 23  # Pin 16 on the board
 SBB_MODULE_ADDR_HOUR = 12
 SBB_MODULE_ADDR_MIN = 1
 
 
 def main() -> None:
-    print("Starting clock!")
+    pir = MotionSensor(PIR_SENSOR_PIN)
+    pir.when_motion = motion_function
+
+    threading.Thread(target=clock_task, daemon=True).start()
+
+    pause()
+
+
+def clock_task() -> None:
+    print("Starting clock task!")
 
     clock = sbb_rs485.PanelClockControl(
         port="/dev/ttyS0", addr_hour=SBB_MODULE_ADDR_HOUR, addr_min=SBB_MODULE_ADDR_MIN
@@ -67,7 +73,7 @@ def main() -> None:
             # sleeptime = 60 - (ts.second + ts.microsecond / 1000000.0)
             # time.sleep(sleeptime)
     except KeyboardInterrupt:
-        print("Exiting program")
+        print("Exiting clock task")
     # finally:
     #     GPIO.cleanup()
 
