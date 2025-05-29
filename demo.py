297c7f290462eded4f1094a7bfae23fa502b86af
Pascal Bérard <pas.berard@gmail.com>
2025-05-29 16:34:11 +0200

    Added models and provided all the model paths manually since 0.6.0 does not ship with models and also doesn't download it at runtime.

--- demo.py
+++ demo.py
@@ -1,183 +1,98 @@
-# /// script
-# requires-python = ">=3.11"
-# dependencies = ["pyserial==3.5", "gpiozero==1.5.1", "pvporcupine==3.0.5", "pvrecorder==1.2.7"]
-# ///
-
+# Copyright 2022 David Scripka. All rights reserved.
 #
-# Copyright 2018-2023 Picovoice Inc.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# You may not use this file except in compliance with the license. A copy of the license is located in the "LICENSE"
-# file accompanying this source.
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
-# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
-# an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
-# specific language governing permissions and limitations under the License.
-#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-import argparse
-import os
-import struct
-import wave
-from datetime import datetime
+# Imports
+import rich.traceback
 
-import pvporcupine
-from pvrecorder import PvRecorder
+rich.traceback.install(show_locals=True)
 
+import pyaudio
+import numpy as np
+from openwakeword.model import Model
 
-def main():
-    parser = argparse.ArgumentParser()
+# The path of a specific model to load
+# model_path = ""
+# model_path = "resources/models/embedding_model.tflite"
+model_path = "resources/models/alexa_v0.1.onnx"
+melspec_model_path = "resources/models/melspectrogram.onnx"
+embedding_model_path = "resources/models/embedding_model.onnx"
 
-    parser.add_argument(
-        "--access_key",
-        help="AccessKey obtained from Picovoice Console (https://console.picovoice.ai/)",
+# The inference framework to use (either 'onnx' or 'tflite')
+# inference_framework = "tflite"
+inference_framework = "onnx"
+
+# How much audio (in number of samples) to predict on at once
+chunk_size = 1280
+
+# Get microphone stream
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 16000
+CHUNK = chunk_size
+audio = pyaudio.PyAudio()
+mic_stream = audio.open(
+    format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK
+)
+
+# Load pre-trained openwakeword models
+if model_path != "":
+    # owwModel = Model(
+    #     wakeword_models=[model_path], inference_framework=inference_framework
+    #     inference_framework=inference_framework,
+    # )
+    owwModel = Model(
+        wakeword_models=[model_path],
+        melspec_model_path=melspec_model_path,
+        embedding_model_path=embedding_model_path,
+        inference_framework=inference_framework,
     )
+else:
+    owwModel = Model(inference_framework=inference_framework)
 
-    parser.add_argument(
-        "--keywords",
-        nargs="+",
-        help="List of default keywords for detection. Available keywords: %s"
-        % ", ".join("%s" % w for w in sorted(pvporcupine.KEYWORDS)),
-        choices=sorted(pvporcupine.KEYWORDS),
-        metavar="",
-    )
+n_models = len(owwModel.models.keys())
 
-    parser.add_argument(
-        "--keyword_paths",
-        nargs="+",
-        help="Absolute paths to keyword model files. If not set it will be populated from `--keywords` argument",
-    )
-
-    parser.add_argument(
-        "--library_path",
-        help="Absolute path to dynamic library. Default: using the library provided by `pvporcupine`",
-    )
-
-    parser.add_argument(
-        "--model_path",
-        help="Absolute path to the file containing model parameters. "
-        "Default: using the library provided by `pvporcupine`",
-    )
-
-    parser.add_argument(
-        "--sensitivities",
-        nargs="+",
-        help="Sensitivities for detecting keywords. Each value should be a number within [0, 1]. A higher "
-        "sensitivity results in fewer misses at the cost of increasing the false alarm rate. If not set 0.5 "
-        "will be used.",
-        type=float,
-        default=None,
-    )
-
-    parser.add_argument(
-        "--audio_device_index",
-        help="Index of input audio device.",
-        type=int,
-        default=-1,
-    )
-
-    parser.add_argument(
-        "--output_path",
-        help="Absolute path to recorded audio for debugging.",
-        default=None,
-    )
-
-    parser.add_argument("--show_audio_devices", action="store_true")
-
-    args = parser.parse_args()
-
-    if args.show_audio_devices:
-        for i, device in enumerate(PvRecorder.get_available_devices()):
-            print("Device %d: %s" % (i, device))
-        return
-
-    if args.keyword_paths is None:
-        if args.keywords is None:
-            raise ValueError("Either `--keywords` or `--keyword_paths` must be set.")
-
-        keyword_paths = [pvporcupine.KEYWORD_PATHS[x] for x in args.keywords]
-    else:
-        keyword_paths = args.keyword_paths
-
-    if args.sensitivities is None:
-        args.sensitivities = [0.5] * len(keyword_paths)
-
-    if len(keyword_paths) != len(args.sensitivities):
-        raise ValueError(
-            "Number of keywords does not match the number of sensitivities."
-        )
-
-    try:
-        porcupine = pvporcupine.create(
-            access_key=args.access_key,
-            library_path=args.library_path,
-            model_path=args.model_path,
-            keyword_paths=keyword_paths,
-            sensitivities=args.sensitivities,
-        )
-    except pvporcupine.PorcupineInvalidArgumentError as e:
-        print("One or more arguments provided to Porcupine is invalid: ", args)
-        print(e)
-        raise e
-    except pvporcupine.PorcupineActivationError as e:
-        print("AccessKey activation error")
-        raise e
-    except pvporcupine.PorcupineActivationLimitError as e:
-        print(
-            "AccessKey '%s' has reached it's temporary device limit" % args.access_key
-        )
-        raise e
-    except pvporcupine.PorcupineActivationRefusedError as e:
-        print("AccessKey '%s' refused" % args.access_key)
-        raise e
-    except pvporcupine.PorcupineActivationThrottledError as e:
-        print("AccessKey '%s' has been throttled" % args.access_key)
-        raise e
-    except pvporcupine.PorcupineError as e:
-        print("Failed to initialize Porcupine")
-        raise e
-
-    keywords = list()
-    for x in keyword_paths:
-        keyword_phrase_part = os.path.basename(x).replace(".ppn", "").split("_")
-        if len(keyword_phrase_part) > 6:
-            keywords.append(" ".join(keyword_phrase_part[0:-6]))
-        else:
-            keywords.append(keyword_phrase_part[0])
-
-    print("Porcupine version: %s" % porcupine.version)
-
-    recorder = PvRecorder(
-        frame_length=porcupine.frame_length, device_index=args.audio_device_index
-    )
-    recorder.start()
-
-    wav_file = None
-    if args.output_path is not None:
-        wav_file = wave.open(args.output_path, "w")
-        wav_file.setnchannels(1)
-        wav_file.setsampwidth(2)
-        wav_file.setframerate(16000)
-
-    print("Listening ... (press Ctrl+C to exit)")
-
-    try:
-        while True:
-            pcm = recorder.read()
-            result = porcupine.process(pcm)
-
-            if wav_file is not None:
-                wav_file.writeframes(struct.pack("h" * len(pcm), *pcm))
-
-            if result >= 0:
-                print("[%s] Detected %s" % (str(datetime.now()), keywords[result]))
-    except KeyboardInterrupt:
-        print("Stopping ...")
-    finally:
-        recorder.delete()
-        porcupine.delete()
-        if wav_file is not None:
-            wav_file.close()
-
-
+# Run capture loop continuosly, checking for wakewords
 if __name__ == "__main__":
-    main()
+    # Generate output string header
+    print("\n\n")
+    print("#" * 100)
+    print("Listening for wakewords...")
+    print("#" * 100)
+    print("\n" * (n_models * 3))
+
+    while True:
+        # Get audio
+        audio = np.frombuffer(mic_stream.read(CHUNK), dtype=np.int16)
+
+        # Feed to openWakeWord model
+        prediction = owwModel.predict(audio)
+
+        # Column titles
+        n_spaces = 16
+        output_string_header = """
+            Model Name         | Score | Wakeword Status
+            --------------------------------------
+            """
+
+        for mdl in owwModel.prediction_buffer.keys():
+            # Add scores in formatted table
+            scores = list(owwModel.prediction_buffer[mdl])
+            curr_score = format(scores[-1], ".20f").replace("-", "")
+
+            output_string_header += f"""{mdl}{" "*(n_spaces - len(mdl))}   | {curr_score[0:5]} | {"--"+" "*20 if scores[-1] <= 0.5 else "Wakeword Detected!"}
+            """
+
+        # Print results table
+        print("\033[F" * (4 * n_models + 1))
+        print(output_string_header, "                             ", end="\r")
