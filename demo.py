3d4f7dafff354108419d0dbc45a60d12d73f0caf
Pascal Bérard <pas.berard@gmail.com>
2025-10-25 00:14:15 +0200

    Added wake word callback to WakeWordDetector

--- demo.py
+++ demo.py
@@ -9,6 +9,8 @@ rich.traceback.install(show_locals=True)
 
 class WakeWordDetector:
     def __init__(self) -> None:
+        self._detection_threshold = 0.1
+
         self._WAKE_WORD_MODEL_PATHS: list[str] = [
             # "resources/models/alexa_v0.1.onnx",
             # "resources/models/custom/hey_clock.onnx",
@@ -58,11 +60,17 @@ class WakeWordDetector:
 
         self._model_count: int = len(self._model.models.keys())
 
+        self._wake_word_callback = None
+
+    def register_wake_word_callback(self, callback):
+        """Register a callback to be called when a wake word is detected."""
+        self._wake_word_callback = callback
+
     def listen_for_wake_word(self) -> None:
         # Generate output string header
         print("\n\n")
         print("#" * 100)
-        print("Listening for wakewords...")
+        print("Listening for wake words...")
         print("#" * 100)
         print("\n" * (self._model_count * 3))
 
@@ -81,17 +89,25 @@ class WakeWordDetector:
             # Column titles
             spaces_count = 16
             output_string_header = """
-                Model Name         | Score | Wake word Status
+                Model Name         | Score | Wake Word Status
                 ---------------------------------------------
                 """
 
+            wake_word_detected = False
             for m in self._model.prediction_buffer.keys():
                 # Add scores in formatted table
                 scores: list = list(self._model.prediction_buffer[m])
                 current_score: str = format(scores[-1], ".20f").replace("-", "")
 
-                output_string_header += f"""{m}{" "*(spaces_count - len(m))}   | {current_score[0:5]} | {"--"+" "*20 if scores[-1] <= 0.1 else "Wakeword Detected!"}
+                detected = scores[-1] > self._detection_threshold
+                if detected:
+                    wake_word_detected = True
+
+                output_string_header += f"""{m}{" "*(spaces_count - len(m))}   | {current_score[0:5]} | {"--"+" "*20 if not detected else "Wake Word Detected!"}
                 """
+
+            if wake_word_detected and self._wake_word_callback:
+                self._wake_word_callback()
 
             # Print results table
             print("\033[F" * (4 * self._model_count + 1))
@@ -100,5 +116,11 @@ class WakeWordDetector:
 
 if __name__ == "__main__":
     wake_word_detector = WakeWordDetector()
+
+    def wake_word_handler() -> None:
+        # print("Wake word callback triggered!")
+        pass
+
+    wake_word_detector.register_wake_word_callback(wake_word_handler)
 
     wake_word_detector.listen_for_wake_word()
