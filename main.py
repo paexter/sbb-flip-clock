17cd97fc3e67382bee671636fdc7eeeaf5dbf26f
Pascal Bérard <pas.berard@gmail.com>
2026-02-08 20:30:54 +0100

    Disabled demo mode

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from clock import Clock
 
 if __name__ == "__main__":
-    clock = Clock(addr_hour=27, addr_min=1, enable_demo_mode=True)
+    clock = Clock(addr_hour=27, addr_min=1, enable_demo_mode=False)
     clock.run()
